d17f4353345e8b9d97f59592cdca33e046c24123
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-20 21:19:58 +0530

    Pump some power to dex2oatd
    Change-Id: I6686bcb50843778c03254ed4a46663201e4e313c

--- product/perf.mk
+++ product/perf.mk
@@ -7,3 +7,10 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.max_starting_bg=8 \
     ro.sys.fw.use_trim_settings=true
+
+ifeq ($(TARGET_DEVICE),jalebi)
+PRODUCT_PROPERTY_OVERRIDES += \
+   dalvik.vm.boot-dex2oat-threads=8
+   dalvik.vm.dex2oat-threads=6
+   dalvik.vm.image-dex2oat-threads=8
+endif
