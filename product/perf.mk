290bc2fc2ccb7e0b21ae5ee175783a6ed6a8201e
Harsh Shandilya <msfjarvis@gmail.com>
2017-02-04 15:17:40 +0530

    perf : Fix-up and extend dex2oat ramp up
    Change-Id: I0a0953940bded3a415983d9a6eac8565d1ef78b5

--- product/perf.mk
+++ product/perf.mk
@@ -8,9 +8,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.max_starting_bg=8 \
     ro.sys.fw.use_trim_settings=true
 
-ifeq ($(TARGET_DEVICE),jalebi)
+ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.boot-dex2oat-threads=8
-   dalvik.vm.dex2oat-threads=6
-   dalvik.vm.image-dex2oat-threads=8
+   dalvik.vm.boot-dex2oat-threads=8 \
+   dalvik.vm.dex2oat-threads=6 \
+   dalvik.vm.image-dex2oat-threads=8 \
+   ro.sys.fw.dex2oat_thread_count=8
 endif
