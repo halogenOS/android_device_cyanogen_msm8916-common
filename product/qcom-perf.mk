10a2cceaba1aa7ddf6a6d68cbdb7562cea8e277c
Harsh Shandilya <msfjarvis@gmail.com>
2017-04-12 22:05:27 +0530

    [3/3] Nuke perfd
    Change-Id: I226317a279c4375510d581e9bd7021dcb268426c

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -9,9 +9,6 @@ PRODUCT_PACKAGES += \
     power.msm8916
 
 # Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
-
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=200000
